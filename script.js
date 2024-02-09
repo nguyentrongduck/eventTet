1ba755a001ef866956e50d374d1415a8d985559f
Nguyễn Trọng Đức <139199678+nguyentrongduck@users.noreply.github.com>
2024-02-09 19:25:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,256 @@
+/**
+ * Prize data will space out evenly on the deal wheel based on the amount of items available.
+ * @param text [string] name of the prize
+ * @param color [string] background color of the prize
+ * @param reaction ['resting' | 'dancing' | 'laughing' | 'shocked'] Sets the reaper's animated reaction
+ */
+const prizes = [
+  {
+    text: "2 cái nịt",
+    color: "hsl(197 30% 43%)",
+    reaction: "dancing"
+  },
+  { 
+    text: "2.000đ",
+    color: "hsl(173 58% 39%)",
+    reaction: "shocked"
+  },
+  { 
+    text: "10.000đ",
+    color: "hsl(43 74% 66%)",
+    reaction: "shocked" 
+  },
+  {
+    text: "Cái nịt",
+    color: "hsl(27 87% 67%)",
+    reaction: "shocked"
+  },
+  {
+    text: "1000đ",
+    color: "hsl(12 76% 61%)",
+    reaction: "dancing"
+  },
+  {
+    text: "Chúc bạn may mắn lần sau",
+    color: "hsl(350 60% 52%)",
+    reaction: "laughing"
+  },
+  {
+    text: "500đ",
+    color: "hsl(91 43% 54%)",
+    reaction: "laughing"
+  },
+  {
+    text: "5000đ",
+    color: "hsl(140 36% 74%)",
+    reaction: "dancing"
+  }
+];
+
+const wheel = document.querySelector(".deal-wheel");
+const spinner = wheel.querySelector(".spinner");
+const trigger = wheel.querySelector(".btn-spin");
+const ticker = wheel.querySelector(".ticker");
+const reaper = wheel.querySelector(".grim-reaper");
+const prizeSlice = 360 / prizes.length;
+const prizeOffset = Math.floor(180 / prizes.length);
+const spinClass = "is-spinning";
+const selectedClass = "selected";
+const spinnerStyles = window.getComputedStyle(spinner);
+let tickerAnim;
+let rotation = 0;
+let currentSlice = 0;
+let prizeNodes;
+
+const createPrizeNodes = () => {
+  prizes.forEach(({ text, color, reaction }, i) => {
+    const rotation = ((prizeSlice * i) * -1) - prizeOffset;
+    
+    spinner.insertAdjacentHTML(
+      "beforeend",
+      `<li class="prize" data-reaction=${reaction} style="--rotate: ${rotation}deg">
+        <span class="text">${text}</span>
+      </li>`
+    );
+  });
+};
+
+const createConicGradient = () => {
+  spinner.setAttribute(
+    "style",
+    `background: conic-gradient(
+      from -90deg,
+      ${prizes
+        .map(({ color }, i) => `${color} 0 ${(100 / prizes.length) * (prizes.length - i)}%`)
+        .reverse()
+      }
+    );`
+  );
+};
+
+
+const setupWheel = () => {
+  createConicGradient();
+  createPrizeNodes();
+  prizeNodes = wheel.querySelectorAll(".prize");
+};
+
+const spinertia = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const runTickerAnimation = () => {
+  // https://css-tricks.com/get-value-of-css-rotation-through-javascript/
+  const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
+  const a = values[0];
+  const b = values[1];  
+  let rad = Math.atan2(b, a);
+  
+  if (rad < 0) rad += (2 * Math.PI);
+  
+  const angle = Math.round(rad * (180 / Math.PI));
+  const slice = Math.floor(angle / prizeSlice);
+
+  if (currentSlice !== slice) {
+    ticker.style.animation = "none";
+    setTimeout(() => ticker.style.animation = null, 10);
+    currentSlice = slice;
+  }
+
+  tickerAnim = requestAnimationFrame(runTickerAnimation);
+};
+
+const selectPrize = () => {
+  const selected = Math.floor(rotation / prizeSlice);
+  prizeNodes[selected].classList.add(selectedClass);
+  reaper.dataset.reaction = prizeNodes[selected].dataset.reaction;
+};
+
+trigger.addEventListener("click", () => {
+  if (reaper.dataset.reaction !== "resting") {
+    reaper.dataset.reaction = "resting";
+  }
+
+  trigger.disabled = true;
+  rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
+  prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
+  wheel.classList.add(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+  ticker.style.animation = "none";
+  runTickerAnimation();
+});
+
+spinner.addEventListener("transitionend", () => {
+  cancelAnimationFrame(tickerAnim);
+  trigger.disabled = false;
+  trigger.focus();
+  rotation %= 360;
+  selectPrize();
+  wheel.classList.remove(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+});
+
+setupWheel();
+
+/* ------------------------ Watermark (Please Ignore) ----------------------- */
+const createSVG = (width, height, className, childType, childAttributes) => {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  const child = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    childType
+  );
+
+  for (const attr in childAttributes) {
+    child.setAttribute(attr, childAttributes[attr]);
+  }
+
+  svg.appendChild(child);
+
+  return { svg, child };
+};
+
+document.querySelectorAll(".generate-button").forEach((button) => {
+  const width = button.offsetWidth;
+  const height = button.offsetHeight;
+
+  const style = getComputedStyle(button);
+
+  const strokeGroup = document.createElement("div");
+  strokeGroup.classList.add("stroke");
+
+  const { svg: stroke } = createSVG(width, height, "stroke-line", "rect", {
+    x: "0",
+    y: "0",
+    width: "100%",
+    height: "100%",
+    rx: parseInt(style.borderRadius, 10),
+    ry: parseInt(style.borderRadius, 10),
+    pathLength: "30"
+  });
+
+  strokeGroup.appendChild(stroke);
+  button.appendChild(strokeGroup);
+
+  const stars = gsap.to(button, {
+    repeat: -1,
+    repeatDelay: 0.5,
+    paused: true,
+    keyframes: [
+      {
+        "--generate-button-star-2-scale": ".5",
+        "--generate-button-star-2-opacity": ".25",
+        "--generate-button-star-3-scale": "1.25",
+        "--generate-button-star-3-opacity": "1",
+        duration: 0.3
+      },
+      {
+        "--generate-button-star-1-scale": "1.5",
+        "--generate-button-star-1-opacity": ".5",
+        "--generate-button-star-2-scale": ".5",
+        "--generate-button-star-3-scale": "1",
+        "--generate-button-star-3-opacity": ".5",
+        duration: 0.3
+      },
+      {
+        "--generate-button-star-1-scale": "1",
+        "--generate-button-star-1-opacity": ".25",
+        "--generate-button-star-2-scale": "1.15",
+        "--generate-button-star-2-opacity": "1",
+        duration: 0.3
+      },
+      {
+        "--generate-button-star-2-scale": "1",
+        duration: 0.35
+      }
+    ]
+  });
+
+  button.addEventListener("pointerenter", () => {
+    gsap.to(button, {
+      "--generate-button-dots-opacity": "1",
+      duration: 0.5,
+      onStart: () => {
+        setTimeout(() => stars.restart().play(), 500);
+      }
+    });
+  });
+
+  button.addEventListener("pointerleave", () => {
+    gsap.to(button, {
+      "--generate-button-dots-opacity": "0",
+      "--generate-button-star-1-opacity": ".25",
+      "--generate-button-star-1-scale": "1",
+      "--generate-button-star-2-opacity": "1",
+      "--generate-button-star-2-scale": "1",
+      "--generate-button-star-3-opacity": ".5",
+      "--generate-button-star-3-scale": "1",
+      duration: 0.15,
+      onComplete: () => {
+        stars.pause();
+      }
+    });
+  });
+});
