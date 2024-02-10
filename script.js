536aadc5c88e93c311fd5a7ad595b7966c2b3732
Nguyễn Trọng Đức <139199678+nguyentrongduck@users.noreply.github.com>
2024-02-10 13:30:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,14 @@
  */
 
 document.addEventListener('DOMContentLoaded', function() {
-import 'https://tomashubelbauer.github.io/github-pages-local-storage/index.js';
+const setItem = localStorage.setItem;
+localStorage.constructor.prototype.setItem = (key, value) => setItem.apply(localStorage, [location.pathname + ':' + key, value])
+
+const getItem = localStorage.getItem;
+localStorage.constructor.prototype.getItem = (key) => getItem.apply(localStorage, [location.pathname + ':' + key]);
+
+const removeItem = localStorage.removeItem;
+localStorage.constructor.prototype.removeItem = (key) => removeItem.apply(localStorage, [location.pathname + ':' + key]);
 const prizes = [
   {
     text: "2 cái nịt",
